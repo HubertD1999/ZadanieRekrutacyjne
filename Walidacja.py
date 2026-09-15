faf48bd8bfc4c6bfbc1ef6b8ab08fdd07b5f2f0b
HubertD1999 <dmowski.hubert99@gmail.com>
2026-09-15 14:29:45 +0200

    Version 1.1

--- Walidacja.py
+++ Walidacja.py
@@ -27,7 +27,7 @@ for i in paths:
     missing_names = validator.check_missing_attribute("Name")
     if len(missing_names)>0:
         print(f"Brakujące nazwy w {i}:\n")
-        for element in missing_names:
+        for element in missing_names[:5]:
             print(
                 element.id(),
                 element.is_a(),
@@ -53,3 +53,26 @@ for i in paths:
                 element.Name
             )
         print("\n")
+
+"Sprawdzam poprawność przypisania poziomów"
+
+for i in paths:
+    validator = IFCValidator(ifcopenshell.open(i))
+    wrong, intersecting = validator.check_element_levels()
+
+    '''print(f"Błędnie przypisane w {i}: {len(wrong)}")
+    print(f"Przecinające level w {i}: {len(intersecting)}")
+
+    for item in intersecting:
+        element = item["element"]
+
+        print(
+            element.id(),
+            element.is_a(),
+            element.GlobalId,
+            "Level:", item["level"],
+            "Element Z:",
+            item["element_min_z"],
+            "-",
+            item["element_max_z"]
+        )'''
